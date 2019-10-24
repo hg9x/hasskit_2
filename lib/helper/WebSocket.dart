ca307f7fb59914729817c9126d7cc3815da64749
Hoang Anh Tuan <tuanha2000vn@yahoo.com>
2019-10-25 03:28:47 +0700

    Nice mechanic send 1 get_states 5 seconds after button spam to get the updated correct entity status

--- lib/helper/WebSocket.dart
+++ lib/helper/WebSocket.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:hasskit_2/helper/GeneralData.dart';
@@ -142,8 +143,30 @@ class WebSocket {
         _channel.sink.add(message);
 //        log.d('WebSocket send: id $id type $type $message');
         gd.socketIdIncrement();
+
+        runMultipleTimes();
       }
     }
+  }
+
+  //https://stackoverflow.com/questions/17552757/is-there-any-way-to-cancel-a-dart-future
+  Timer _runJustOnceAtTheEnd;
+
+  void runMultipleTimes() {
+    _runJustOnceAtTheEnd?.cancel();
+    _runJustOnceAtTheEnd = null;
+
+    // do your processing
+//    print("runMultipleTimes!");
+
+    _runJustOnceAtTheEnd = Timer(Duration(seconds: 5), onceAtTheEndOfTheBatch);
+  }
+
+  void onceAtTheEndOfTheBatch() {
+    var outMsg = {"id": gd.socketId, "type": "get_states"};
+    send(json.encode(outMsg));
+    gd.connectionStatus = "Sending get_states";
+    log.w("Sending get_states 5 seconds after the last send spam!");
   }
 
   /// ---------------------------------------------------------
